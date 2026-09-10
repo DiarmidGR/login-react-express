3d9ee9f08d621cd48d3a35affeae00567933cd44
DiarmidGR <diarmid@hotmail.ca>
2026-09-10 01:04:02 -0600

    refactored for readability

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -8,45 +8,34 @@ import CheckboxComponent from "../../components/Checkbox/Checkbox";
 import {setToken} from "../../utils/Auth";
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState(() => {
-    return localStorage.getItem("remembered_username") || "";
-  });
+  // Get username from remembered_username if available, initialize empty otherwise. 
+  const [username, setUsername] = useState(() => localStorage.getItem("remembered_username") || "");
   const [password, setPassword] = useState("");
-  const [rememberUser, setRememberUser] = useState(() => {
-    return localStorage.getItem("remembered_username") !== null;
-  });
+  // Get rememberUser from localStorage if available, initialize false otherwise.
+  const [rememberUser, setRememberUser] = useState(() => localStorage.getItem("remembered_username") !== null);
   const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+  const navigate = useNavigate();
 
-  // Code to limit toasts on screen to 1, obtained from https://stackoverflow.com/a/72932186
-  const {toasts} = useToasterStore();
+  // Limit the number of visible toasts to 1 (To limit spam issue i had before.)
+  const { toasts } = useToasterStore();
   const TOAST_LIMIT = 1;
+
   useEffect(() => {
-    toasts
-      .filter((t) => t.visible) // Only consider visible toasts
-      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit
-      .forEach((t) => toast.remove(t.id)); // Remove the toast
+    toasts.filter((t) => t.visible).filter((_, i) => i >= TOAST_LIMIT).forEach((t) => toast.remove(t.id));
   }, [toasts]);
 
-  let navigate = useNavigate();
-
-  // Code to show error message toast if user was redirected from a token refresh error
-  const searchParams = new URLSearchParams(window.location.search);
   useEffect(() => {
-    if (searchParams.get('message') === 'refresh-error') {
+    if (new URLSearchParams(window.location.search).get('message') === 'refresh-error') {
       toast.error('Session expired. Login to continue.');
     }
   }, []);
 
-  // Login handler
-  const handleSubmit = async (e: any) => {
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const res = await axios.post(
         `${import.meta.env.VITE_API_ENDPOINT}/login`,
-        {
-          username,
-          password,
-        },
+        { username, password },
         { withCredentials: true }
       );
       setToken(res.data.accessToken);
@@ -67,17 +56,14 @@ const Login: React.FC = () => {
 
   return (
     <div className="login-layout">
-      {/* Toaster component to display toast notifications */}
-      <Toaster/>
+      <Toaster />
       <div className="login-container">
-        <form onSubmit={handleSubmit} className="login-form">
+        <form onSubmit={handleLogin} className="login-form">
           <h1 className="login-header switzer-bold">Login to Your Account</h1>
-          <h3 className="login-subheader switzer-regular">
-            Login to track user progress.
-          </h3>
+          <h3 className="login-subheader switzer-regular">Login to track user progress.</h3>
           <label htmlFor="" className="login-child">
             <input
-              type="username"
+              type="text"
               className="login-input switzer-regular"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
@@ -85,7 +71,6 @@ const Login: React.FC = () => {
               placeholder="Username"
             />
           </label>
-
           <label htmlFor="" className="login-child">
             <input
               type="password"
@@ -101,36 +86,17 @@ const Login: React.FC = () => {
               isChecked={rememberUser}
               setIsChecked={setRememberUser}
             />
-            <label className="remember-label switzer-regular">
-              Remember this user
-            </label>
+            <label className="remember-label switzer-regular">Remember this user</label>
           </div>
-          <button type="submit" className="login-button switzer-bold">
-            Sign In
-          </button>
-
+          <button type="submit" className="login-button switzer-bold">Sign In</button>
         </form>
       </div>
       <div className="register-container">
         <h1 className="switzer-bold">No account?</h1>
-        <p className="register-message switzer-regular">
-          To track across multiple devices:
-        </p>
-        <button
-         className="switzer-bold"
-         onClick={()=> setIsRegisterOpen(true)}
-         >
-          Register User
-        </button>
-        <p className="register-message switzer-regular">
-          To track on this device only:
-        </p>
-        <button
-          className="guest-button switzer-bold"
-          onClick={() => navigate("/")}
-        >
-          Continue as Guest
-        </button>
+        <p className="register-message switzer-regular">To track across multiple devices:</p>
+        <button className="switzer-bold" onClick={() => setIsRegisterOpen(true)}>Register User</button>
+        <p className="register-message switzer-regular">To track on this device only:</p>
+        <button className="guest-button switzer-bold" onClick={() => navigate("/")}>Continue as Guest</button>
       </div>
       <RegisterModal
         isOpen={isRegisterOpen}
@@ -138,6 +104,5 @@ const Login: React.FC = () => {
       />
     </div>
   );
-}
-
+};
 export default Login;
