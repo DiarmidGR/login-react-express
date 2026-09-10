7429dfaf7e105f53f7a4c71f888234362cdc72a7
DiarmidGR <diarmid@hotmail.ca>
2026-09-10 12:00:14 -0600

    refactored with tailwindcss

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
-import "./Home.css";
 import { getUsername, isAuthenticated } from "../../utils/Auth";
 
 const Home: React.FC = () => {
   return (
-    <div className="home-layout">
-      <div className="home-container">
+    <div className="home-layout flex flex-col w-full">
+      <div className="home-container flex text-[var(--third-color)] justify-center items-center h-full">
         {isAuthenticated() ? <p>Logged in: {getUsername()}</p> : <p>Not logged in</p>}
       </div>
     </div>
